bc748fe9f424bf7dfb31346f108d7b3d4283c44d
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 21:08:37 -0600

    Update foundry-hot-seat-observer.js

--- foundry-hot-seat-observer.js
+++ foundry-hot-seat-observer.js
@@ -1,23 +1,3 @@
-Hooks.on('init', () => {
-    game.settings.register("foundry-hot-seat-observer", "hotSeatObserver", {
-        name: "Hot Seat Observer",
-        hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-        scope: "world",
-        config: true,
-        default: true,
-        type: Boolean,
-    });
-    
-    game.settings.register("foundry-hot-seat-observer", "toggleGMSelect", {
-		name: game.i18n.localize("fhso.toggleGMSelect.name"),
-		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-		scope: "world",
-		config: true,
-		default: false,
-		type: Boolean
-	});
-});
-
 Hooks.on('updateCombat', (data, opt) => {
 
     if (game.settings.get('foundry-hot-seat-observer', 'hotSeatObserver')) {
