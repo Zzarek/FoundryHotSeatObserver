732bf601ba99d372689be13dfd653e378387a9cd
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 20:14:05 -0600

    Update worldSettings.js

--- worldSettings.js
+++ worldSettings.js
@@ -9,8 +9,8 @@ Hooks.once("init", () => {
 	* Register better rolls setting
 	*/
 	game.settings.register("foundryhotseatobserver", "toggleGMSelect", {
-		name: i18n("fhso.diceEnabled.name"),
-		hint: i18n("fhso.diceEnabled.hint"),
+		name: i18n("fhso.toggleGMSelect.name"),
+		hint: i18n("fhso.toggleGMSelect.hint"),
 		scope: "world",
 		config: true,
 		default: false,
@@ -18,8 +18,8 @@ Hooks.once("init", () => {
 	});
   
   	game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
-		name: i18n("br5e.hotSeatPlayerName.name"),
-		hint: i18n("br5e.hotSeatPlayerName.hint"),
+		name: i18n("fhso.hotSeatPlayerName.name"),
+		hint: i18n("fhso.hotSeatPlayerName.hint"),
 		scope: "world",
 		config: true,
 		default: true,
