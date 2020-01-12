df2761f14789d45a4b9c1e41936bc311f04bf2da
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 21:03:18 -0600

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,16 +1,13 @@
 Hooks.on("init", () => {
 
-	/**
-	* Register better rolls setting
-	*/
-	game.settings.register("foundryhotseatobserver", "toggleGMSelect", {
-		name: game.i18n.localize("fhso.toggleGMSelect.name"),
-		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-		scope: "world",
-		config: true,
-		default: false,
-		type: Boolean
-	});
+//	game.settings.register("foundryhotseatobserver", "toggleGMSelect", {
+//		name: game.i18n.localize("fhso.toggleGMSelect.name"),
+//		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
+//		scope: "world",
+//		config: true,
+//		default: false,
+//		type: Boolean
+//	});
   
   	game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
 		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
