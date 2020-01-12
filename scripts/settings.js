dbf9f7f342769bf39e3c00540b1e1a30cb10cdce
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 21:42:31 -0600

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -18,12 +18,12 @@ Hooks.on('init', () => {
     });
   
   game.settings.register("foundry-hot-seat-observer", "hotSeatPlayerName", {
-		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
-		hint: game.i18n.localize("fhso.hotSeatPlayerName.hint"),
-		scope: "world",
-		config: true,
-		default: true,
-		type: String
-	});
+	name: "PlayerName",
+	hint: "Player",
+	scope: "world",
+	config: true,
+	default: true,
+	type: String
+  });
 
 });
