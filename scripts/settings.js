9a3b99bebb03d47ca2057e145caa5665c9e73de4
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 21:09:11 -0600

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,15 +1,23 @@
-Hooks.on("init", () => {
-
-//	game.settings.register("foundryhotseatobserver", "toggleGMSelect", {
-//		name: game.i18n.localize("fhso.toggleGMSelect.name"),
-//		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-//		scope: "world",
-//		config: true,
-//		default: false,
-//		type: Boolean
-//	});
+Hooks.on('init', () => {
+    game.settings.register("foundry-hot-seat-observer", "hotSeatObserver", {
+        name: "Hot Seat Observer",
+        hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
+        scope: "world",
+        config: true,
+        default: true,
+        type: Boolean,
+    });
+    
+    game.settings.register("foundry-hot-seat-observer", "toggleGMSelect", {
+		name: game.i18n.localize("fhso.toggleGMSelect.name"),
+		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean
+	});
   
-  	game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
+  game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
 		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
 		hint: game.i18n.localize("fhso.hotSeatPlayerName.hint"),
 		scope: "world",
