58fd2d0c5db86b460fefde4db30120fd89c1d8bd
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 21:12:23 -0600

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,29 +1,29 @@
 Hooks.on('init', () => {
     game.settings.register("foundry-hot-seat-observer", "hotSeatObserver", {
-        name: "Hot Seat Observer",
-        hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-        scope: "world",
-        config: true,
-        default: true,
-        type: Boolean,
+       	name: "Hot Seat Observer",
+       	hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
+       	scope: "world",
+       	config: true,
+       	default: true,
+	type: Boolean,
     });
     
-    game.settings.register("foundry-hot-seat-observer", "toggleGMSelect", {
+//    game.settings.register("foundry-hot-seat-observer", "toggleGMSelect", {
 		name: game.i18n.localize("fhso.toggleGMSelect.name"),
-		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-		scope: "world",
-		config: true,
-		default: false,
-		type: Boolean
-	});
+//		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
+//		scope: "world",
+//		config: true,
+//		default: false,
+//		type: Boolean
+//	});
   
-  game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
-		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
-		hint: game.i18n.localize("fhso.hotSeatPlayerName.hint"),
-		scope: "world",
-		config: true,
-		default: true,
-		type: String
-	});
+//  game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
+//		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
+//		hint: game.i18n.localize("fhso.hotSeatPlayerName.hint"),
+//		scope: "world",
+//		config: true,
+//		default: true,
+//		type: String
+//	});
 
 });
