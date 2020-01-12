0dd91097092cceee0409650fabe813ff58a8f085
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-11 21:27:07 -0600

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -8,22 +8,22 @@ Hooks.on('init', () => {
 	type: Boolean,
     });
     
-//    game.settings.register("foundry-hot-seat-observer", "toggleGMSelect", {
-//		name: game.i18n.localize("fhso.toggleGMSelect.name"),
-//		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
-//		scope: "world",
-//		config: true,
-//		default: false,
-//		type: Boolean
-//	});
+    game.settings.register("foundry-hot-seat-observer", "toggleGMSelect", {
+		name: game.i18n.localize("fhso.toggleGMSelect.name"),
+		hint: game.i18n.localize("fhso.toggleGMSelect.hint"),
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean
+	});
   
-//  game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
-//		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
-//		hint: game.i18n.localize("fhso.hotSeatPlayerName.hint"),
-//		scope: "world",
-//		config: true,
-//		default: true,
-//		type: String
-//	});
+  game.settings.register("foundryhotseatobserver", "hotSeatPlayerName", {
+		name: game.i18n.localize("fhso.hotSeatPlayerName.name"),
+		hint: game.i18n.localize("fhso.hotSeatPlayerName.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: String
+	});
 
 });
