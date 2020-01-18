9f6abbc679898238fcdaafe2238108ae483cde24
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-17 21:02:09 -0600

    Update GMSHotSeatDriver.js

--- scripts/GMSHotSeatDriver.js
+++ scripts/GMSHotSeatDriver.js
@@ -1,41 +1,24 @@
 class GMSHotSeatDriver {
 
 	static patchFunction(func, line_number, line, new_line) {
-
 		let funcStr = func.toString()
-
 		let lines = funcStr.split("\n")
-
 		if (lines[line_number].trim() == line.trim()) {
-
 			let fixed = funcStr.replace(line, new_line)
-
 			return Function('"use strict";return (function ' + fixed + ')')();
-
 		}
-
 		return func;
-
 	}
 
 	static init() {
-
 		Canvas.prototype._onMouseDown = Deselection.patchFunction(
-
 			Canvas.prototype._onMouseDown,
-
 			23,
-
 			"event.data._selectState = 1;",
-
 			`if (canvas.ready && Object.keys(canvas.activeLayer._controlled).length) canvas.activeLayer.releaseAll();
-
 			event.data._selectState = 1;`
-
 		);
-
 	}
-
 }
 
 
