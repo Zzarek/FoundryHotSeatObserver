8ca65c233991bb0ce544a9575d6cb8b47e08366a
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-18 14:12:41 -0600

    Update gm-hot-seat-driver.js

--- scripts/gm-hot-seat-driver.js
+++ scripts/gm-hot-seat-driver.js
@@ -1,4 +1,7 @@
 Hooks.on("controlToken", (token, isControlled) => {
+      if (game.settings.get('foundry-hot-seat-observer', 'hotSeatObserver') && game.settings.get('foundry-hot-seat-observer', 'toggleGMSelect')) {
+
+      }
 
 	let x = 2;
 });
