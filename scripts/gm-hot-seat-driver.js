27a86404fe9285dccba3ffcf1b9476084b036b48
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-18 20:57:06 -0600

    Update gm-hot-seat-driver.js

--- scripts/gm-hot-seat-driver.js
+++ scripts/gm-hot-seat-driver.js
@@ -1,10 +1,18 @@
 Hooks.on("controlToken", (token, isControlled) => {
 	  let gameSettingsEnabled = game.settings.get('foundry-hot-seat-observer', 'hotSeatObserver') && game.settings.get('foundry-hot-seat-observer', 'toggleGMSelect');
-	  let tokenViableForChange = token.actor.isPC;
+	  let tokenViableForChange = token.actor && token.actor.isPC;
       if (gameSettingsEnabled && tokenViableForChange && game.user.isGM === true) {
-	
+	        let hotSeatPlayerName = game.settings.get('foundry-hot-seat-observer', 'hotSeatPlayerName')
+            let hotSeatPlayerUser = game.users.players.find(t => t.name === hotSeatPlayerName);
 
-	            let x = 2;
+            if(hotSeatPlayerUser && isControlled){
+               hotSeatPlayerUser.update({character: token.actor.id});              
+            }
+            else if(hotSeatPlayerUser && !isControlled){
+                
+            }
+            //let.game
+
 
       	
 
