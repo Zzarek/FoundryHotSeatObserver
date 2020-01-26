b8c06bd0acc6210feb16225509bb349e2dfb4a86
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-01-25 20:26:06 -0600

    Update gm-hot-seat-driver.js

--- scripts/gm-hot-seat-driver.js
+++ scripts/gm-hot-seat-driver.js
@@ -6,14 +6,9 @@ Hooks.on("controlToken", (token, isControlled) => {
             let hotSeatPlayerUser = game.users.players.find(t => t.name === hotSeatPlayerName);
 
             if(hotSeatPlayerUser){
-              	hotSeatPlayerUser.update({character: token.actor.id}, {controlled: isControlled});
+              	hotSeatPlayerUser.update({character: token.actor.id, dummy: isControlled}, {controlled: isControlled, character: token.actor.id});
             }
 	
-	    //token.actor.update({}, {user: hotSeatPlayerUser});
-	    token.actor.update({}, {token: token._id});
-
-		
-		
 	}
 });
 
@@ -22,24 +17,14 @@ Hooks.on("controlToken", (token, isControlled) => {
 Hooks.on("updateUser", (user, updateData, options, userId) => {
 	let gameSettingsEnabled = game.settings.get('foundry-hot-seat-observer', 'hotSeatObserver') && game.settings.get('foundry-hot-seat-observer', 'toggleGMSelect');
 	let isCorrectUser = game.user._id == updateData._id && !game.user.isGM && game.user.name == game.settings.get('foundry-hot-seat-observer', 'hotSeatPlayerName');
-	if(gameSettingsEnabled && isCorrectUser && updateData.hasOwnProperty("character") && options.hasOwnProperty("controlled")){
-		const actor = game.actors.get(updateData.character);
+	if(gameSettingsEnabled && isCorrectUser && options.hasOwnProperty("character") && options.hasOwnProperty("controlled")){
+		const actor = game.actors.get(options.character);
     		const tokens = actor.getActiveTokens();
     		if ( tokens.length && options.controlled ) tokens[0].control();
 		else if (tokens.length && !options.controlled) tokens[0].release();	
 	}
 });
 
-Hooks.on("updateActor", (actor, actorData, options, actorId) => {
-	let gameSettingsEnabled = game.settings.get('foundry-hot-seat-observer', 'hotSeatObserver') && game.settings.get('foundry-hot-seat-observer', 'toggleGMSelect');
-	//let isCorrectUser = game.user._id == updateData._id && !game.user.isGM && game.user.name == game.settings.get('foundry-hot-seat-observer', 'hotSeatPlayerName');
-	if(gameSettingsEnabled && options.hasOwnProperty("token")){
-	    let hotSeatPlayerName = game.settings.get('foundry-hot-seat-observer', 'hotSeatPlayerName')
-            let hotSeatPlayerUser = game.users.players.find(t => t.name === hotSeatPlayerName);
-		
-		
-	}
-});
 
 
 
