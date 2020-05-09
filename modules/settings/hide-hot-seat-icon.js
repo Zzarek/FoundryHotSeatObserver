dcc6cc78dd8b6f567c823b6212d896168f3cec0d
Zzarek <31546035+Zzarek@users.noreply.github.com>
2020-05-09 15:11:55 -0500

    COntinued testing

--- modules/settings/hide-hot-seat-icon.js
+++ modules/settings/hide-hot-seat-icon.js
@@ -8,18 +8,23 @@ export class HideIconMode{
         let hotSeatPlayerName = Settings.RetreiveHotSeatPlayerName();
         let hotSeatPlayerUser = game.users.players.find(t => t.name === hotSeatPlayerName);
 
-        let pcs = game.actors.entities.filter(t => t.isPC && t.data.permission[hotSeatPlayerUser.id] == "2");
+        let pcs = game.actors.entities.filter(t => t.isPC && t.data.permission[hotSeatPlayerUser.id] == "3");
 
-        pcs.array.forEach(element => {
-            
+
+        let pcTokens = game.tokens.entities.filter(t => t.actor.isPC && t.actor.data.permission[hotSeatPlayerUser.id] == "3");
+        let isUserGm = game.user.isGM;
+
+        pcs.forEach(element => {
+            if(!isUserGm)
+                element.data.token.icon.visible = gameSettingsEnabled;
         });
 
 
-        let tokenViableForChange = token.actor && token.actor.isPC;
-        let isUserGm = game.user.isGM;
-        if (!isUserGm && tokenViableForChange && gameSettingsEnabled === true) {
-            token.icon.visible = false;
-        }
+        // let tokenViableForChange = token.actor && token.actor.isPC;
+        // let isUserGm = game.user.isGM;
+        // if (!isUserGm && tokenViableForChange && gameSettingsEnabled === true) {
+        //     token.icon.visible = false;
+        // }
         
     }
 
